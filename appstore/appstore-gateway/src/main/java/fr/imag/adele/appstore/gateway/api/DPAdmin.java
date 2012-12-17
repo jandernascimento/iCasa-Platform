58a3b856625755a9e82a8cdee881227611c20bde
torito <torito@gmail.com>
2012-12-17 15:11:35 +0100

    add initial commit for Deployment Package Admin with API REST

---
+++ appstore/appstore-gateway/src/main/java/fr/imag/adele/appstore/gateway/api/DPAdmin.java
@@ -0,0 +1,12 @@
+package fr.imag.adele.appstore.gateway.api;
+
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Instantiate;
+
+@Component
+@Instantiate
+public class DPAdmin {
+
+	
+
+}
