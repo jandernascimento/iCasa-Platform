34e96a4e1b5e6e56494c795072b51fbad3b6df82
torito <torito@gmail.com>
2012-12-17 17:29:36 +0100

    Add rest api to deployment package

--- appstore/appstore-gateway/src/main/java/fr/imag/adele/appstore/gateway/api/DPAdmin.java
+++ appstore/appstore-gateway/src/main/java/fr/imag/adele/appstore/gateway/api/DPAdmin.java
@@ -1,12 +1,66 @@
 package fr.imag.adele.appstore.gateway.api;
 
+import java.util.Collections;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
 import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Provides;
+import org.apache.felix.ipojo.annotations.Requires;
+import org.apache.felix.ipojo.annotations.StaticServiceProperty;
+import org.osgi.service.deploymentadmin.DeploymentAdmin;
+import org.osgi.service.deploymentadmin.DeploymentPackage;
+import org.ow2.chameleon.json.JSONService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Component
-@Instantiate
+import fr.imag.adele.appstore.gateway.util.DPResourceManipulator;
+
+
+@Component(name="deployment-admin-rest-api")
+@Instantiate(name="deployment-admin-rest-api-0")
+@Provides(specifications={DPAdmin.class}, properties= {
+		@StaticServiceProperty(name="appstore.exporter.protocol", type="java.lang.String", value="jax-rs")})
+@Path(value="/")
 public class DPAdmin {
 
+	@Requires
+	private JSONService jsonservice; //JsonService, in order to parse request and build reponses
 	
+	@Requires
+	private DeploymentAdmin dadmin;
+	
+	protected static Logger coreLogger = LoggerFactory.getLogger("appstore.logger");
 
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/applications/")
+	public Response getInstalledApplications() {
+		DeploymentPackage[] installed = dadmin.listDeploymentPackages();
+		List installedInfo = DPResourceManipulator.toList(installed);
+		String result = jsonservice.toJSON(Collections.singletonMap("installedPackages", installedInfo));
+		return Response.ok(result).build();
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/applications/{name}")
+	public Response getApplication(@PathParam("name")String name) {
+		DeploymentPackage ipackage = dadmin.getDeploymentPackage(name);
+		if (ipackage == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		String result = jsonservice.toJSON(DPResourceManipulator.toMap(ipackage));
+		return Response.ok(result).build();
+	}
+	
 }
---
+++ appstore/appstore-gateway/src/main/java/fr/imag/adele/appstore/gateway/util/DPResourceManipulator.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright Adele Team LIG
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.imag.adele.appstore.gateway.util;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+import org.osgi.service.deploymentadmin.DeploymentPackage;
+
+/**
+ * @author <a href="mailto:cilia-devel@lists.ligforge.imag.fr">Cilia Project
+ *         Team</a>
+ */
+public class DPResourceManipulator {
+
+	
+	public static Map<String, String> toMap(DeploymentPackage dpackage){
+		Map<String, String> resultMap = new Hashtable<String, String>();
+		resultMap.put("version", dpackage.getVersion().getQualifier());
+		resultMap.put("displayName", dpackage.getDisplayName());
+		resultMap.put("name", dpackage.getName());
+		if(dpackage.getIcon() != null) {
+			resultMap.put("iconUrl", dpackage.getIcon().toExternalForm());
+		}
+		return resultMap;
+	}
+	
+	public static List <Map<String, String>> toList(DeploymentPackage[] packages) {
+		List<Map<String, String>> resultList = new ArrayList<Map<String, String>>();
+		for (DeploymentPackage dpackage : packages) {
+			resultList.add(DPResourceManipulator.toMap(dpackage));
+		}
+		return resultList;
+	}
+}
